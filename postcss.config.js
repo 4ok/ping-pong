8efacf2fc9804fc7ec96c0a4b7cef9ef5e9f8f0c
4ok <4ok.dev@gmail.com>
2018-10-07 20:06:48 +0300

    fixed the postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production'
-const prodPlugin = (name, params = {}) => (isProd ? { [name]: params } : {})
 
 module.exports = {
 	plugins: {
 		autoprefixer: { grid: true },
-		...prodPlugin('cssnano'),
+		cssnano: isProd,
 	},
 }
